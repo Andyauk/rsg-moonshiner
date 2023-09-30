f292f8951baa0e88960c97aab83cfe02ff9d49e3
RexshackGaming <rexshackuk@gmail.com>
2023-09-30 14:23:06 +0100

    version checker

--- server/server.lua
+++ server/server.lua
@@ -1,5 +1,36 @@
 local RSGCore = exports['rsg-core']:GetCoreObject()
 
+-----------------------------------------------------------------------
+-- version checker
+-----------------------------------------------------------------------
+local function versionCheckPrint(_type, log)
+    local color = _type == 'success' and '^2' or '^1'
+
+    print(('^5['..GetCurrentResourceName()..']%s %s^7'):format(color, log))
+end
+
+local function CheckVersion()
+    PerformHttpRequest('https://raw.githubusercontent.com/Rexshack-RedM/rsg-moonshiner/main/version.txt', function(err, text, headers)
+        local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version')
+
+        if not text then 
+            versionCheckPrint('error', 'Currently unable to run a version check.')
+            return 
+        end
+
+        --versionCheckPrint('success', ('Current Version: %s'):format(currentVersion))
+        --versionCheckPrint('success', ('Latest Version: %s'):format(text))
+        
+        if text == currentVersion then
+            versionCheckPrint('success', 'You are running the latest version.')
+        else
+            versionCheckPrint('error', ('You are currently running an outdated version, please update to version %s'):format(text))
+        end
+    end)
+end
+
+-----------------------------------------------------------------------
+
 -- use moonshine kit
 RSGCore.Functions.CreateUseableItem("moonshinekit", function(source, item)
     local Player = RSGCore.Functions.GetPlayer(source)
@@ -58,3 +89,8 @@ AddEventHandler('rsg-moonshiner:server:startsmoke', function(coords)
     local src = source
     TriggerClientEvent('rsg-moonshiner:client:startsmoke', -1, coords)
 end)
+
+--------------------------------------------------------------------------------------------------
+-- start version check
+--------------------------------------------------------------------------------------------------
+CheckVersion()
